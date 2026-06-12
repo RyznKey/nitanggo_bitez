fd3e821906e0ca723ce2faa4c9c709013f0f993c
diyasramadhan <diyasramadhan25@gmail.com>
2026-06-12 09:49:19 +0700

    .hapus line convig di vercel.json dan nambah file php.ini

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
-// Mengarahkan request Vercel ke index.php bawaan Laravel
+// api/index.php
+
+// Buat folder temporary untuk compiled views supaya tidak memicu error 500
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
+// Lanjutkan booting Laravel
 require __DIR__ . '/../public/index.php';
